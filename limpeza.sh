b768d9e927626bc5c569550a33bbaa6e6768888a
Edson Doria <81723126+EdsonDoria@users.noreply.github.com>
2024-02-18 06:42:45 -0300

    Inicio do script
    Para limpeza do Fedora. Sejam bem vindos para melhorar.

---
+++ limpeza.sh
@@ -0,0 +1,160 @@
+#!/bin/zsh
+#Limpar o sistema
+
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##                     Limpar o sistema                     ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##             Limpar o Systemd - coredumps                 ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
+read resposta
+case "$resposta" in
+  s|S) rm /var/lib/systemd/coredump/* >/dev/null 2>&1 ;;
+  n|N) echo "proximo comando para ser executado" ;;
+  *) exit 0 ;;
+esac
+    for i in {1..10}; do
+        echo -n "######"
+        sleep 0.5
+    done
+#read resp
+#if [ $resp. != 's.' ]; then
+#	exit 0
+#fi
+#rm /var/lib/systemd/coredump/*
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##  Colocando o tempo de armazenamento de logs para 2 dias  ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+journalctl --vacuum-time=2d >/dev/null 2>&1
+
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##          Atribuindo espaço mínimo para logs              ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+journalctl --vacuum-size=500M
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##       Flatpaks não usados seram desisntalados            ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+flatpak uninstall --unused
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##        Limpar cache/buffer, dentries e inodes            ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+sync; echo 3 > /proc/sys/vm/drop_caches
+echo " "
+echo " "
+echo "##############################################################"
+echo "##                                                          ##"
+echo "##       Limpar o espaço em disco consumido pelo SWAP       ##"
+echo "##                                                          ##"
+echo "##############################################################"
+echo " "
+echo " "
+echo "se não tiver memória o suficiente vai travar..."
+echo " "
+echo " "
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+swapoff -a && swapon -a
+echo "--------------------"
+echo "--------------------"
+echo "Remove todos os pacotes do sistema que foram originalmente instalados "
+echo "como dependências de outros pacotes instalados pelo usuário, mas que não são mais exigido por outros programas."
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+dnf autoremove
+
+echo "Executa a limpeza de arquivos temporários mantidos para repositórios. "
+echo "Isso inclui quaisquer dados deixados por trás de repositórios desativados ou removidos"
+echo "bem como para diferentes versões de lançamento de distribuição."
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+dnf clean dbcache
+
+echo "Remove arquivos de cache gerados a partir dos metadados do repositório."
+echo "Isso força o DNF a regenerar os arquivos de cache na próxima vez que ele for executado."
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+dnf clean expire-cache
+
+echo "Remove metadados do repositório."
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+dnf clean packages
+
+echo "Atualiza o sistema todo a partir dos repositórios habilitados."
+echo "Digite s para continuar ou qualquer outra coisa para terminar"
+read resp
+if [ $resp. != 's.' ]; then
+    exit 0
+fi
+dnf distro-sync 
+
+#Exibe um histórico do que foi feito pelo dnf
+#dnf history
