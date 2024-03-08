7bd2c78c91e1cfba32bcf71b7c6c602ff9a449f3
EdsonDoria <fenix.se@gmail.com>
2024-03-08 07:56:01 -0300

    Registrando saída com o "output"

--- limpeza.sh
+++ limpeza.sh
@@ -16,16 +16,42 @@ echo "##############################################################"
 echo " "
 echo " "
 echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
-read resposta1
-case "$resposta1" in
-  s|S) rm /var/lib/systemd/coredump/* >/dev/null 2>&1 ;;
-  n|N) echo "proximo comando para ser executado" ;;
-  *) exit 0 ;;
+#stty -echo # Desliga o eco da entrada do usuário
+read -n resposta
+stty echo # Liga o eco da entrada do usuário
+case "$resposta" in
+    s|S)
+        echo "" # Adiciona uma quebra de linha após a entrada para "Sim"
+        if output=$(rm -fr /var/lib/systemd/coredump/* 2>/dev/null); then
+            echo "Arquivos removidos."
+        else
+            if [[ "$output" != *"No such file or directory"* ]]; then
+                echo "Não existem arquivos para remover."
+                echo "Nada a fazer."
+            fi
+    fi ;;
+    n|N)
+    echo "Próximo comando a ser executado" ;;
+    *)
+    exit 0 ;;
 esac
-    for i in {1..10}; do
-        echo -n "######"
-        sleep 0.5
-    done
+
+for i in {1..10}; do
+    echo -n "######"
+    sleep 0.5
+done
+#Procurando estabelecer o padrão de pergunta e resposta
+#echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
+#read resposta1
+#case "$resposta1" in
+#  s|S) rm /var/lib/systemd/coredump/* >/dev/null 2>&1 ;;
+#  n|N) echo "proximo comando para ser executado" ;;
+#  *) exit 0 ;;
+#esac
+#    for i in {1..10}; do
+#        echo -n "######"
+#        sleep 0.5
+#    done
 #read resp
 #if [ $resp. != 's.' ]; then
 #	exit 0
@@ -43,14 +69,14 @@ echo " "
 echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
 read resposta2
 case "$resposta2" in
-  s|S) journalctl --vacuum-time=2d >/dev/null 2>&1/* ;;
-  n|N) echo "proximo comando para ser executado" ;;
-  *) exit 0 ;;
+    s|S) journalctl --vacuum-time=2d >/dev/null 2>&1/* ;;
+    n|N) echo "proximo comando para ser executado" ;;
+    *) exit 0 ;;
 esac
-    for i in {1..10}; do
-        echo -n "######"
-        sleep 0.5
-    done
+for i in {1..10}; do
+    echo -n "######"
+    sleep 0.5
+done
 #echo "Digite s para continuar ou qualquer outra coisa para terminar"
 #read resp
 #if [ $resp. != 's.' ]; then
@@ -70,14 +96,14 @@ echo " "
 echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
 read resposta3
 case "$resposta3" in
-  s|S) journalctl --vacuum-size=500M >/dev/null 2>&1/* ;;
-  n|N) echo "proximo comando para ser executado" ;;
-  *) exit 0 ;;
+    s|S) journalctl --vacuum-size=500M >/dev/null 2>&1/* ;;
+    n|N) echo "proximo comando para ser executado" ;;
+    *) exit 0 ;;
 esac
-    for i in {1..10}; do
-        echo -n "######"
-        sleep 0.5
-    done
+for i in {1..10}; do
+    echo -n "######"
+    sleep 0.5
+done
 #echo "Digite s para continuar ou qualquer outra coisa para terminar"
 #read resp
 #if [ $resp. != 's.' ]; then
@@ -96,14 +122,14 @@ echo " "
 echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
 read resposta4
 case "$resposta4" in
-  s|S)  flatpak uninstall --unused>/dev/null 2>&1/* ;;
-  n|N) echo "proximo comando para ser executado" ;;
-  *) exit 0 ;;
+    s|S)  flatpak uninstall --unused>/dev/null 2>&1/* ;;
+    n|N) echo "proximo comando para ser executado" ;;
+    *) exit 0 ;;
 esac
-    for i in {1..10}; do
-        echo -n "######"
-        sleep 0.5
-    done
+for i in {1..10}; do
+    echo -n "######"
+    sleep 0.5
+done
 #echo "Digite s para continuar ou qualquer outra coisa para terminar"
 #read resp
 #if [ $resp. != 's.' ]; then
@@ -122,14 +148,14 @@ echo " "
 echo "Pressione 's' para Sim, 'n' para Não ou qualquer outra tecla para sair."
 read resposta5
 case "$resposta5" in
-  s|S)  sync; echo 3 > /proc/sys/vm/drop_caches ;;
-  n|N) echo "proximo comando para ser executado" ;;
-  *) exit 0 ;;
+    s|S)  sync; echo 3 > /proc/sys/vm/drop_caches ;;
+    n|N) echo "proximo comando para ser executado" ;;
+    *) exit 0 ;;
 esac
-    for i in {1..10}; do
-        echo -n "######"
-        sleep 0.5
-    done
+for i in {1..10}; do
+    echo -n "######"
+    sleep 0.5
+done
 #echo "Digite s para continuar ou qualquer outra coisa para terminar"
 #read resp
 #if [ $resp. != 's.' ]; then
@@ -198,7 +224,7 @@ read resp
 if [ $resp. != 's.' ]; then
     exit 0
 fi
-dnf distro-sync 
+dnf distro-sync
 
 #Exibe um histórico do que foi feito pelo dnf
 #dnf history
